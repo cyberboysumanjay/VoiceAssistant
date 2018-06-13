7e4bb87186323c5d4f399464be266dddcc7c5bfe
Cyberboy <cyberboysumanjay@gmail.com>
2018-06-13 23:17:31 +0530

    Handled many inputs manually, added pyaudio_fix.py to get all audio devices and updated requirements

--- va.py
+++ va.py
@@ -6,25 +6,30 @@ import os #file handling
 import pyperclip #clipping
 #import pyttsx3 as tts #text to speech_recognition
 import win32com.client as wincl
+from datetime import datetime
 
 v=wincl.Dispatch("SAPI.SpVoice")
-#cl=wolframalpha.Client('YVH8AY-R8H93LQAJ2')
-#att=cl.query('Test/Attempt')
+cl=wolframalpha.Client('YVH8AY-R8H93LQAJ2')
+att=cl.query('Test/Attempt')
 r=sr.Recognizer()
 r.pause_threshold=0.7
 r.energy_threshold=500
 shell=wincl.Dispatch("WScript.Shell")
 #v.Speak('Hello! For a list of commands, please say "Keyword list"...')
 v.Speak('At your service Sir!')
-print('Hello! For a list of commands, please say "Keyword list"...')
+#print('Hello! For a list of commands, please say "Keyword list"...')
 
 #List of commands
 google = 'search for'
+youtube='search YouTube for'
 acad = 'academic search'
-sc = 'deep search'
 wkp = 'wiki page for'
-rdds = 'read this text'
-sav = 'save this text'
+rdds = 'read the text'
+t='what is the time'
+d='what is the date'
+say='say'
+copy='copy the text'
+sav = 'save the text'
 bkmk = 'bookmark this page'
 vid = 'video for'
 wtis = 'what is'
@@ -38,14 +43,18 @@ paint = 'open paint'
 lsp = 'silence please'
 lsc = 'resume listening'
 stoplst = 'stop listening'
+sc = 'deep search'
+
 
 while True:
     with sr.Microphone() as source:
         try:
-            print("Please Speak")
+            v.Speak("How can I help you today?")
+            print("Waiting for your command")
             audio=r.listen(source,timeout=4)
             message=str(r.recognize_google(audio))
             print('You said: '+message)
+            v.Speak('Hmmmmmmm.. Seems Interesting')
             if google in message:
                 words=message.split()
                 del words[0:2]
@@ -67,9 +76,55 @@ while True:
                 del words[0:3]
                 st=' '.join(words)
                 print("Wikipedia Page of : "+str(st))
-                url='https://en.wikipedia.org/w/index.php?search='+st
+                url='https://en.wikipedia.org/w/index.php?search='+str(st)
                 webbrowser.open(url)
                 v.Speak('Wiki Page for: '+str(st))
+            elif rdds in message:
+                words=message.split()
+                del words[0:3]
+                st=' '.join(words)
+                print("Reading the text: "+str(st))
+                v.Speak('Alright reading the text: '+ str(st))
+            elif say in message:
+                words=message.split()
+                del words[0:1]
+                st=' '.join(words)
+                print("Repeating the text: "+str(st))
+                v.Speak('Alright, Saying..: '+ str(st))
+            elif copy in message:
+                words=message.split()
+                del words[0:3]
+                st=' '.join(words)
+                pyperclip.copy(st)
+                print("The text "+str(st)+" is now copied to clipboard!")
+                v.Speak('The text ..'+str(st)+' is now in your clipboard... Happy Pasting!')
+            elif youtube in message:
+                words=message.split()
+                del words[0:3]
+                st=' '.join(words)
+                print("Searching for "+str(st)+" on Youtube")
+                url='https://www.youtube.com/results?search_query='+str(st)
+                webbrowser.open(url)
+                v.Speak('Youtube Search results for: '+str(st))
+            elif vid in message:
+                words=message.split()
+                del words[0:2]
+                st=' '.join(words)
+                print("Searching for "+str(st)+" on Youtube")
+                url='https://www.youtube.com/results?search_query='+str(st)
+                webbrowser.open(url)
+                v.Speak('Youtube Search results for: '+str(st))
+            elif t in message:
+                c=time.ctime()
+                words=c.split()
+                v.Speak("The time is: "+str(words[3]))
+            elif d in message:
+                c=time.ctime()
+                words=c.split()
+                v.Speak("Today, it is"+words[2]+words[1]+words[4])
+            else:
+                res = client.query(message)
+                v.Speak(res)
         except:
             break
         finally:
